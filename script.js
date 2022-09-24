0329220b59171ccc3629e98c93cc0dc7f8748242
Caio Figueirôa <cfigueiroa@gmail.com>
2022-09-23 21:11:24 -0300

    Working

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let container = document.querySelector("#container")
-let quantity = 16*16
+let container = document.querySelector("#container");
+let quantity = 16 * 16;
 
-for (let i = quantity; i > 0; i--){
-    let div = document.createElement('div');
-    div.classList.add("pixel");
-    container.appendChild(div);
+for (let i = quantity; i > 0; i--) {
+  let div = document.createElement("div");
+  div.classList.add("pixel");
+  container.appendChild(div);
 }
 
+pixel = document.querySelectorAll("div.pixel");
 
+pixel.forEach((element) =>
+  element.addEventListener("mouseover", (e) => {
+      e.toElement.style.backgroundColor = "black";
+  })
+);
