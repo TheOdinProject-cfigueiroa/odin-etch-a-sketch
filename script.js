f4e3264044c105c749b2be163d0691f98bc22f95
Caio Figueirôa <cfigueiroa@gmail.com>
2022-09-24 10:05:35 -0300

    make content flexible

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let container = document.querySelector("#container");
-let quantity = 16 * 16;
+let quantity = 16;
 
 for (let i = quantity; i > 0; i--) {
-  let div = document.createElement("div");
-  div.classList.add("pixel");
-  container.appendChild(div);
+  let line = document.createElement("div");
+  line.classList.add("line");
+  for (let j = quantity; j > 0; j--) {
+    let div = document.createElement("div");
+    div.classList.add("pixel");
+    line.appendChild(div);
+  }
+  container.appendChild(line);
 }
 
 pixel = document.querySelectorAll("div.pixel");
 
 pixel.forEach((element) =>
   element.addEventListener("mouseover", (e) => {
-      e.toElement.style.backgroundColor = "black";
+    e.toElement.style.backgroundColor = "black";
   })
 );
