3773da6d3e57acd68a5b0cd218871d2f3d26eaa8
Caio Figueirôa <cfigueiroa@gmail.com>
2022-09-24 11:59:52 -0300

    it's working
    pure magic

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-let container = document.querySelector("#container");
-let quantity = 16;
-
-for (let i = quantity; i > 0; i--) {
-  let line = document.createElement("div");
-  line.classList.add("line");
-  for (let j = quantity; j > 0; j--) {
-    let div = document.createElement("div");
-    div.classList.add("pixel");
-    line.appendChild(div);
+function buildCanvas(quantity = 16) {
+  let container = document.querySelector("#container");
+  container.innerHTML = "";
+  for (let i = quantity; i > 0; i--) {
+    let line = document.createElement("div");
+    line.classList.add("line");
+    for (let j = quantity; j > 0; j--) {
+      let div = document.createElement("div");
+      div.classList.add("pixel");
+      line.appendChild(div);
+    }
+    container.appendChild(line);
   }
-  container.appendChild(line);
+  pixel = document.querySelectorAll("div.pixel");
+
+  pixel.forEach((element) =>
+    element.addEventListener("mouseover", (e) => {
+      e.toElement.style.backgroundColor = "black";
+    })
+  );
 }
 
-pixel = document.querySelectorAll("div.pixel");
+btn = document.querySelector("button");
 
-pixel.forEach((element) =>
-  element.addEventListener("mouseover", (e) => {
-    e.toElement.style.backgroundColor = "black";
-  })
-);
+btn.addEventListener("click", (e) => {
+  buildCanvas(Number(prompt("how many?")));
+});
+
+buildCanvas();
